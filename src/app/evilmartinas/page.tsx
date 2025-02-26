4b835393de3e758cf7021a414de2c0b0962d1146
Gribanov_German <germangribanov@mail.ru>
2025-02-26 08:59:36 +0000

    refactor: grammatical errors

--- src/app/evilmartinas/page.tsx
+++ src/app/evilmartinas/page.tsx
@@ -2,7 +2,7 @@ const EvilMartinasPage = () => {
   return (
     <section>
       <div className="container">
-        Evil Martinas & German Gribanov & Expected adventure...
+        Evil Martians & German Gribanov & Expected adventure...
       </div>
     </section>
   );
