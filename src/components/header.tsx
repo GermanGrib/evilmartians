e22e95492fdfb83c1ed16824e99f4ba39311d48a
Gribanov_German <germangribanov@mail.ru>
2025-03-01 17:00:49 +0000

    refactor: icons sizes & styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,10 +10,10 @@ const Header = () => {
   const pathName = usePathname() || "";
 
   const linkCommonStyle =
-    "hidden px-5 py-4 rounded-xl ring-1 ring-[#E6E6E6] transition lg:flex justify-center items-center lg:hover:bg-[#b097f9] lg:hover:text-white";
+    "px-5 py-4 rounded-xl ring-1 ring-[#E6E6E6] transition  justify-center items-center lg:hover:bg-[#b097f9] lg:hover:text-white";
 
   return (
-    <header className="py-2">
+    <header className="py-2 fixed lg:static bg-white lg:bg-transparent w-full z-50">
       <div className="container flex justify-between items-center">
         <Logo className="w-12 h-12 lg:w-16 lg:h-16 lg:hover:text-em-tertiriary" />
         <nav>
@@ -21,25 +21,25 @@ const Header = () => {
             <li>
               {initialized && (
                 <Link
-                  className={`${linkCommonStyle} ${pathName === RoutesPaths.HOME ? "bg-em-tertiriary text-white" : ""}`}
+                  className={`hidden lg:flex ${linkCommonStyle} ${pathName === RoutesPaths.HOME ? "bg-em-tertiriary text-white" : ""}`}
                   href={RoutesPaths.HOME}
                 >
                   Home
                 </Link>
               )}
             </li>
-            <li className="flex gap-4">
+            <li>
               {isAuthenticated && initialized && (
                 <Link
                   href={RoutesPaths.EVILMARTIANS}
-                  className={`${linkCommonStyle} ${pathName === RoutesPaths.EVILMARTIANS ? "bg-em-tertiriary text-white" : ""}`}
+                  className={`hidden lg:flex ${linkCommonStyle} ${pathName === RoutesPaths.EVILMARTIANS ? "bg-em-tertiriary text-white" : ""}`}
                 >
                   Martinas
                 </Link>
               )}
               {!isAuthenticated && initialized && (
                 <Link
-                  className={`${linkCommonStyle} ${pathName === RoutesPaths.LOGIN ? "bg-em-tertiriary text-white" : ""}`}
+                  className={`hidden lg:flex ${linkCommonStyle} ${pathName === RoutesPaths.LOGIN ? "bg-em-tertiriary text-white" : ""}`}
                   href={RoutesPaths.LOGIN}
                 >
                   Login
@@ -49,7 +49,7 @@ const Header = () => {
             {isAuthenticated && initialized && (
               <li onClick={logout}>
                 <Link
-                  className={`${linkCommonStyle} text-sm lg:text-base`}
+                  className={`${linkCommonStyle} flex text-sm lg:text-base`}
                   href={RoutesPaths.HOME}
                 >
                   Logout
